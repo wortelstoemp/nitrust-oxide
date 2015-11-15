8f931a579f1ba3c52b0c8261ec5fd7f73d18dfd0
Tomikaza <tom.quareme@gmail.com>
2015-11-15 19:10:31 +0100

    Got basic SDL and OpenGL working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 // Copyright 2015 Tom Quareme @wortelstoemp
+// Note that this file currently is a mess
+// When I get basic OpenGL 3.3 running this will evolve
+// into an earlier renderer I wrote in LWJGL back in the day.
+
 extern crate gl;
+extern crate libc;
 extern crate sdl2;
 extern crate time;
+
+use libc::c_void;
+use gl::types::*;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+use core::Clock;
 
 mod core {
 	use time::PreciseTime;
@@ -64,12 +76,47 @@ mod core {
 	}
 }
 
-use core::Clock;
+mod math {
+	pub struct Vec2 {
+		x: f32,
+		y: f32
+	}
+
+    impl Vec2 {
+        pub fn new() -> Vec2 {
+            Vec2 { x: 0.0, y: 0.0 }
+        }
+    }
+}
+
+// struct TestStructA {
+// 	x: i32,
+// 	y: i32,
+// }
+//
+// struct TestStructB {
+// 	t: Option<TestStructA>,
+// 	z: i32,
+// }
+//
+// impl TestStructB {
+// 	fn new() -> TestStructB {
+// 		TestStructB {
+// 			t: None,
+// 			z: 0,
+// 		}
+// 	}
+// }
 
 fn main() {
+	// let mut testyStr: TestStructB = TestStructB::new();
+	// testyStr.t = Some(TestStructA {x: 0, y: 0});
 	// Initialize SDL stuff (later in WindowsSystem)
+
 	let sdl_context = sdl2::init().unwrap();
 	let video_subsystem = sdl_context.video().unwrap();
+	video_subsystem.gl_set_swap_interval(1); // If vsync
+
 	let window = video_subsystem.window("Nitrust Oxide", 800, 600)
 		.position_centered()
 		.opengl()
@@ -77,6 +124,12 @@ fn main() {
 		//.fullscreen()
 		.build()
 		.unwrap();
+	let ctx = window.gl_create_context().unwrap();
+	window.gl_make_current(&ctx).unwrap();
+
+	gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
+
+	let mut event_pump = sdl_context.event_pump().unwrap();
 
 	// Initialize clock
 	let mut running = true;
@@ -87,7 +140,16 @@ fn main() {
 	while running {
 		dt = clock.delta();
 
-		// Do non fixed stuff
+		// Input
+		for event in event_pump.poll_iter() {
+			match event {
+				Event::Quit {..} |
+				Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+					running = false;
+				},
+				_ => {}
+			}
+		}
 
 		while clock.accumulating() {
 			// Do fixed stuff
@@ -98,6 +160,11 @@ fn main() {
 		// println!("fps: {}", (1.0/dt));
 
 		// Do non fixed stuff
+		unsafe {
+            gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
 
+		window.gl_swap_window();
 	}
 }
