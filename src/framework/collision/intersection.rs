434697e500e8fc066c2d07b629a2cc52f0bb21f4
Tomikaza <tom.quareme@gmail.com>
2015-12-08 12:38:35 +0100

    Fixed AABB intersection

--- src/framework/collision/intersection.rs
+++ src/framework/collision/intersection.rs
@@ -2,15 +2,15 @@ use framework::collision::{AABB, Sphere};
 use framework::math::Vec3;
 
 pub fn intersects_AABB(a: AABB, b: AABB) -> bool {
-	if (a.center.x - b.center.x.abs()) > (a.size.x + b.size.x) {
+	if (a.center.x - b.center.x).abs() > (a.size.x + b.size.x) {
 		return false;
 	}
 	
-	if (a.center.y - b.center.y.abs()) > (a.size.y + b.size.y) {
+	if (a.center.y - b.center.y).abs() > (a.size.y + b.size.y) {
 		return false;
 	}
 	
-	if (a.center.z - b.center.z.abs()) > (a.size.z + b.size.z) {
+	if (a.center.z - b.center.z).abs() > (a.size.z + b.size.z) {
 		return false;
 	}
 
